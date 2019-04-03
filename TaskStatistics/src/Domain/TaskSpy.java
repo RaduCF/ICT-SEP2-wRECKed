e5f57c8790ec6d12fbe8aa6877e06d69d514fd43
Christian Desktop <christianstewartsoenderup@hotmail.com>
2019-04-03 22:59:57 +0200

    TaskSpy update

--- TaskStatistics/src/Domain/TaskSpy.java
+++ TaskStatistics/src/Domain/TaskSpy.java
@@ -6,32 +6,51 @@ import java.io.*;
 
 public class TaskSpy {
 
+	ServerSocket serverSocket;
+    Socket socket;
+    InputStream in; 
+	
+    boolean init = false;
+     
+    public void close()
+    {
+    	try {
+            socket.close();	
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    }
+     
     public String run()
     {
 
+    	if(!init)
+    	{
+    		try
+    		{
+                serverSocket = new ServerSocket(5000, 10);
+                socket = serverSocket.accept();
+                in = socket.getInputStream();
+    		} catch (Exception e){}
+    	
+    		init = true;
+    	}
+    	
         try
         {
 
-            ServerSocket serverSocket = new ServerSocket(5000, 10);
-            Socket socket = serverSocket.accept();
-            InputStream in = socket.getInputStream();
-
             String received = "";
-            while (!received.equals("exit"))
-            {
-                // Receiving
-                byte[] lenBytes = new byte[4];
-                in.read(lenBytes, 0, 4);
-                int len = (((lenBytes[3] & 0xff) << 24) | ((lenBytes[2] & 0xff) << 16) |
-                        ((lenBytes[1] & 0xff) << 8) | (lenBytes[0] & 0xff));
-                byte[] receivedBytes = new byte[len];
-                in.read(receivedBytes, 0, len);
-                received = new String(receivedBytes, 0, len);
+            // Receiving
+            byte[] lenBytes = new byte[4];
+            in.read(lenBytes, 0, 4);
+            int len = (((lenBytes[3] & 0xff) << 24) | ((lenBytes[2] & 0xff) << 16) |
+                    ((lenBytes[1] & 0xff) << 8) | (lenBytes[0] & 0xff));
+            byte[] receivedBytes = new byte[len];
+            in.read(receivedBytes, 0, len);
+            received = new String(receivedBytes, 0, len);
 
-                return received;
-            }
+            return received;
 
-            socket.close();
 
         } catch (Exception e){}
 
