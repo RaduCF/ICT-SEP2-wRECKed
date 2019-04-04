d8f8b9b9c8291828d4271211d863fec64a1224d8
Christian Desktop <christianstewartsoenderup@hotmail.com>
2019-04-04 18:28:39 +0200

    round 2

--- TaskStatistics/src/StartUp.java
+++ TaskStatistics/src/StartUp.java
@@ -2,11 +2,7 @@ import static javafx.application.Application.launch;
 
 public class StartUp {
 
-<<<<<<< HEAD
-    public static void main(String[] args){
-=======
     public static void main(String[] args) {
->>>>>>> master
         launch(Main.class);
     }
 }
