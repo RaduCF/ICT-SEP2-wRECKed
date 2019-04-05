92d14a296be33963ff60980db2b353f4888ff95d
Christian Desktop <christianstewartsoenderup@hotmail.com>
2019-04-05 18:25:46 +0200

    update 2

--- TaskStatistics/src/Main.java
+++ TaskStatistics/src/Main.java
@@ -1,3 +1,4 @@
+import Model.Mediator.*;
 import UserViewPack.MainView;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,6 +9,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //needs all the model classes to be initiated here before the view...
+    	
+    	Client client = new Client();
+        ObservableModel model = new ModelManager(client);
+        
+    	
         MainView view = new MainView();
         view.start(primaryStage);
     }
--- TaskStatistics/src/Model/Mediator/ModelManager.java
+++ TaskStatistics/src/Model/Mediator/ModelManager.java
@@ -1,5 +1,43 @@
 package Model.Mediator;
 
-public class ModelManager {
+import java.beans.PropertyChangeListener;
 
+import utility.observer.listener.GeneralListener;
+import utility.observer.subject.PropertyChangeAction;
+import utility.observer.subject.PropertyChangeProxy;
+
+public class ModelManager implements ObservableModel, ClientModel {
+
+    private PropertyChangeAction<Package, Boolean> property;
+    private Client Client;
+
+    public ModelManager(Client Client){
+        this.Client = Client;
+        this.property = new PropertyChangeProxy<>(this);
+    }
+
+	@Override
+	public void addListener(String propertyName, PropertyChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(String propertyName, PropertyChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addListener(PropertyChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(PropertyChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
