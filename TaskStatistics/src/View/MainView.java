304ca5c52a52e5e16ca5f00c1ee66d430a696007
Karolis <karolis.g04@gmail.com>
2019-04-11 11:09:00 +0200

    Fixing my fuckup update
    Me fuck up

--- TaskStatistics/src/View/MainView.java
+++ TaskStatistics/src/View/MainView.java
@@ -12,15 +12,16 @@ import java.io.IOException;
 public class MainView {
     private Stage primaryStage;
     private Login loginView;
+    private UserView userView;
     private MainViewModel mainViewModel;
 
     public MainView(MainViewModel mainViewModel) {
         this.mainViewModel = mainViewModel;
     }
 
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
-        openLoginView();
+        openUserView();
     }
 
     public void openLoginView() {
@@ -35,8 +36,38 @@ public class MainView {
         catch(IOException e){
             e.printStackTrace();
         }
+        
+        if(primaryStage.getScene()!=null)
+        {
+        	primaryStage.getScene().getWindow().hide();
+        }
+        
         primaryStage.setScene(loginView.getScene());
         primaryStage.setTitle(loginView.getTitle());
+        primaryStage.show();
+    }
+    
+    public void openUserView() throws IOException {
+        try {
+
+        }
+        catch (Exception e){
+            e.getStackTrace();
+        }
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("userViewFXML.fxml"));
+        System.out.println("works!");
+        Parent root = loader.load();
+        System.out.println("works!");
+        Scene scene = new Scene(root,900,500);
+        userView= loader.getController();
+        userView.init(this,mainViewModel.getUserViewModel(),scene, "UserViewPack");
+        
+        userView.getScene();
+        
+        primaryStage.setScene(userView.getScene());
+        //primaryStage.setScene();
+        primaryStage.setTitle(userView.getTitle());
         primaryStage.show();
     }
 }
